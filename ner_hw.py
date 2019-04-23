be86fcb093ed4b5bc47e2dad6ac9fd8dd957a37b
Ian Matheson <iama2027@colorado.edu>
2019-04-23 12:41:28 -0600

    opening file

--- ner_hw.py
+++ ner_hw.py
@@ -1,1 +1,16 @@
 #Code that will return an output of named entities
+
+import numpy
+from sklearn.model_selection import train_test_split
+
+file = "gene-trainF18.txt"
+with open(file, 'r') as pf:
+	lines = pf.read().splitlines()
+	for line in lines:
+		words = line.split()
+		#maybe don't need this?? Sentence counter not line #
+		words = words[1:] #getting rid of line # at front
+		for word in words:
+			pass
+			#print(word)
+# traindata = train_test_split()
